ee7f7e892cfd15a8083060cd13945638eaf62a52
ahoang28 <808361@seq.org>
2024-05-16 09:45:02 -0700

    Update Food.java

--- src/Food.java
+++ src/Food.java
@@ -1,3 +1,26 @@
 public class Food
   {
+    private String name;
+    private int energy;
+    private double price;
+
+    public Food(String n, int e, double p)
+    {
+      name = n;
+      energy = e;
+      price = p;
+    }
+
+    public String getName()
+    {
+      return name;
+    }
+    public int getEnergy()
+    {
+      return energy;
+    }
+    public double getPrice()
+    {
+      return price;
+    }
   }
