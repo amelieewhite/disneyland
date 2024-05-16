ea23872b0dec2a7ce87bab7d775f6ac7179899db
amelieewhite <ameliegwhite@gmail.com>
2024-05-16 16:11:31 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import javax.swing.JFrame;
 import javax.swing.JButton;
 public class Main
 {
+    private ArrayList<Person> party = new ArrayList<Person>();
     
     public static void main(String[] args)
     {
