d587835139d5223ec3726d909d6bda27981224f0
ahoang28 <808361@seq.org>
2024-05-16 09:48:15 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,27 +6,9 @@
  */
 public class Main
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class main
-     */
-    public Main()
+    
+    public static void main(String[] args)
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        
     }
 }
