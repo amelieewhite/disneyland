592f4793479488a6b8778123ce7b9e14535df6cd
amelieewhite <ameliegwhite@gmail.com>
2024-05-16 16:15:46 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,12 @@ import javax.swing.JButton;
 public class Main
 {
     private ArrayList<Person> party = new ArrayList<Person>();
+    private double partyBudget;
+
+    for(Person p : party)
+    {
+        partyBudget += p.getBudget();
+    }
     
     public static void main(String[] args)
     {
