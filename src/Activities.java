8402a98da5ec3585a58b3cac8dcec2629addc20e
amelieewhite <ameliegwhite@gmail.com>
2024-05-16 16:54:23 -0400

    Create Activities.java

---
+++ src/Activities.java
@@ -0,0 +1,11 @@
+public class Activities
+  {
+    private String activityName;
+    private int duration;
+
+    public Activities(String n, int d)
+    {
+      activityName = n;
+      duration = d;
+    }
+  }
