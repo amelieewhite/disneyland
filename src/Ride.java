ec0475e2950b507f06f13f4a577b43dda4ab2ccd
amelieewhite <ameliegwhite@gmail.com>
2024-05-16 12:02:39 -0700

    Update Ride.java

--- src/Ride.java
+++ src/Ride.java
@@ -3,13 +3,47 @@ public class Ride
     private String name;
     private int waitTime
     private int moraleBoost;
+    private int negConsequence;
+    private int posConsequence;
     
     public Ride(String n, int time, int boost)
     {
         name = n;
         waitTime = time;
         moraleBoost = boost;
+
     }
+    public String runRide()
+    {
+        int rand = (int) Math.random()*10;
+        if(rand == 0)
+        {
+            return "Whee! That was fun! Morale + " + moraleBoost + "!";
+            morale += moraleBoost;
+        }
+        if(rand == 1)
+        {
+            return "Oh no! You got motion sick :(. Morale - " + moraleBoost + "!";
+            morale -= moraleBoost;
+        }
+        if(rand == 2)
+        {
+            return "Front row! That was awesome! Morale + " + moraleBoost*2 + "!";
+            morale += moraleBoost*2;
+        }
+        if(rand == 3)
+        {
+            return "Uh oh, " + name + " got stuck! 15 minutes lost. Morale - " + moraleBoost*2 + "!";
+        }
+        if(rand == 4)
+        {
+            return "Again, again! Morale + " + moraleBoost + "!";
+            morale += moraleBoost;
+        }
+        if(rand == 5)
+        {
+            return "";
+        }
 //Getters
     public String getName()
     {
