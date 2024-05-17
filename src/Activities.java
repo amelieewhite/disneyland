57d70670f61f028c9a89a4c6522984b62da40864
ahoang28 <808361@seq.org>
2024-05-17 11:01:46 -0700

    Update Activities.java

--- src/Activities.java
+++ src/Activities.java
@@ -2,10 +2,26 @@ public class Activities
   {
     private String activityName;
     private int duration;
+    private int mBoost;
 
-    public Activities(String n, int d)
+    public Activities(String n, int d, int boost)
     {
       activityName = n;
       duration = d;
+      mBoost = boost;
+    }
+
+    //getters
+    public String getName()
+    {
+      return activityName;
+    }
+    public int getDuration()
+    {
+      return duration;
+    }
+    public int getMBoost()
+    {
+      return mBoost;
     }
   }
