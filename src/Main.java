94c7027b08e084f93564df2f782ef410c609f2fd
Amelie White <ameliegwhite@gmail.com>
2024-05-20 11:32:39 -0400

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main
 {
     Park disney = new Park();
-    
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+    }
+
 }
--- src/Park.java
+++ src/Park.java
@@ -9,12 +9,14 @@ import java.util.*;
 public class Park
 {
     Weather w;
-    ArrayList<Lands> lands;
+    Lands[][] map = new Lands[3][3];
     
     
     public Park()
     {
-        Weather = changeWeather();
+        map[2][1] = new Lands("Entrance", "Main entrance to the park");
+        map[1][1] = new Lands("Main Street", "Main street USA, hub of shops");
+        map[0][1] = new Lands("Fantasyland", "fantasyland woo");
         
     }
 }
